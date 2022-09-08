3d720909a96a65231a05bca4792062ccdb3775ac
Elliot Thomas <elliot.thomas@unity3d.com>
2022-09-08 10:13:34 +0100

    chore: tidy up read logic

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 use std::{
-    fs::File,
-    io::{Read, Write},
+    fs::{read_to_string, File},
+    io::Write,
 };
 
 fn main() {
     let input_path = "./data/input/backgrounds.json";
     let output_path = "./data/output/backgrounds.json";
 
-    let mut input_file = match File::open(input_path) {
-        Ok(file) => file,
-        Err(error) => panic!("Failed to open input file: {}", error),
+    let input_data = match read_to_string(input_path) {
+        Ok(data) => data,
+        Err(error) => panic!("Failed to read input data: {}", error),
     };
 
     let mut output_file = match File::create(output_path) {
         Ok(file) => file,
         Err(error) => panic!("Failed to create output file: {}", error),
     };
-
-    let mut input_data = String::new();
-
-    if let Err(error) = input_file.read_to_string(&mut input_data) {
-        panic!("Failed to read input file: {}", error)
-    }
 
     // todo: parse and convert data here
 
