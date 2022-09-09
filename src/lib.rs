155ef1cae8eed239437f62100fd7863e4c5e0df4
Elliot Thomas <elliot.thomas@unity3d.com>
2022-09-09 15:06:50 +0100

    feat: generic converter and custom errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
-use std::{fs, io::Write};
+use std::{
+    fmt, fs,
+    io::{self, Write},
+    result,
+};
 
 use json::JsonValue;
 
-pub fn convert_file(input_path: &str, output_path: &str) -> Result<()> {
-    DummyConverter::new().convert_file(input_path, output_path)
+pub fn convert_file(kind: &str, input_path: &str, output_path: &str) -> Result<()> {
+    Converter::new(kind)?.convert_file(input_path, output_path)
 }
 
-pub trait Convert {
+pub fn convert_string(kind: &str, input: &str) -> Result<String> {
+    Converter::new(kind)?.convert_string(input)
+}
+
+pub fn convert_json(kind: &str, input: JsonValue) -> Result<JsonValue> {
+    Converter::new(kind)?.convert_json(input)
+}
+
+trait Convert {
     fn convert_file(&self, input_path: &str, output_path: &str) -> Result<()> {
         let input_data = fs::read_to_string(input_path)?;
 
@@ -28,6 +40,27 @@ pub trait Convert {
     fn convert_json(&self, input: JsonValue) -> Result<JsonValue>;
 }
 
+pub enum Converter {
+    Dummy,
+}
+
+impl Converter {
+    pub fn new(kind: &str) -> Result<Converter> {
+        match kind {
+            "dummy" => Ok(Converter::Dummy),
+            _ => Err(Error::UnknownKind(kind.to_string())),
+        }
+    }
+}
+
+impl Convert for Converter {
+    fn convert_json(&self, input: JsonValue) -> Result<JsonValue> {
+        match self {
+            Converter::Dummy => DummyConverter::new().convert_json(input),
+        }
+    }
+}
+
 struct DummyConverter {}
 
 impl DummyConverter {
@@ -42,4 +75,33 @@ impl Convert for DummyConverter {
     }
 }
 
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+pub type Result<T> = result::Result<T, Error>;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(io::Error),
+    JsonError(json::Error),
+    UnknownKind(String),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::IoError(error) => write!(f, "io error: {}", error),
+            Error::JsonError(error) => write!(f, "json error: {}", error),
+            Error::UnknownKind(kind) => write!(f, "unknown kind: {}", kind),
+        }
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::IoError(error)
+    }
+}
+
+impl From<json::Error> for Error {
+    fn from(error: json::Error) -> Self {
+        Error::JsonError(error)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-fn main() {
+fn main() -> retool::Result<()> {
+    let data_kind = "dummy";
     let input_path = "./data/input/backgrounds.json";
     let output_path = "./data/output/backgrounds.json";
 
-    retool::convert_file(input_path, output_path).expect("Convert failed");
+    retool::convert_file(data_kind, input_path, output_path)
 }
