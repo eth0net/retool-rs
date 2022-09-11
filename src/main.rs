8c16dd641c35f2db6a7dd8fac82c1d5b3729ebf0
Elliot Thomas <elliot.thomas@unity3d.com>
2022-09-12 00:14:04 +0100

    feat: implement command line args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,43 @@
+use std::{env, process};
+
 fn main() -> retool::Result<()> {
-    let data_kind = "dummy";
-    let input_path = "./data/input/backgrounds.json";
-    let output_path = "./data/output/backgrounds.json";
+    let args: Vec<String> = env::args().collect();
 
-    let kind = match data_kind {
-        "dummy" => retool::Converter::Dummy,
-        _ => panic!("Unknown kind: {}", data_kind),
-    };
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Failed to build config: {}", err);
+        process::exit(1)
+    });
 
-    retool::convert_file(kind, input_path, output_path)
+    retool::convert_file(config.converter, &config.input, &config.output)
+}
+
+struct Config {
+    converter: retool::Converter,
+    input: String,
+    output: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let kind = args[1].clone();
+        let input = args[2].clone();
+        let output = args[3].clone();
+
+        let converter = match kind.as_str() {
+            "dummy" => retool::Converter::Dummy,
+            _ => {
+                return Err("unknown kind");
+            }
+        };
+
+        Ok(Config {
+            converter,
+            input,
+            output,
+        })
+    }
 }
