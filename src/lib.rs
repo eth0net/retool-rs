a1bfea9b8cd267d32f42275645804aef4221b980
Elliot Thomas <elliot.thomas@unity3d.com>
2022-09-11 20:43:30 +0100

    chore: kind enum and struct converter

--- src/lib.rs
+++ src/lib.rs
@@ -8,23 +8,36 @@ use json::JsonValue;
 
 /// Convert a JSON file containing an array of 5e.tools entries to the Reroll equivalent.
 /// Any resulting data will writen to a JSON file created at the output path.
-pub fn convert_file(kind: &str, input_path: &str, output_path: &str) -> Result<()> {
-    Converter::new(kind)?.convert_file(input_path, output_path)
+pub fn convert_file(kind: ConverterKind, input_path: &str, output_path: &str) -> Result<()> {
+    Converter::new(kind).convert_file(input_path, output_path)
 }
 
 /// Convert serialised JSON array of 5e.tools entries to the Reroll equivalent.
-pub fn convert_string(kind: &str, input: &str) -> Result<String> {
-    Converter::new(kind)?.convert_string(input)
+pub fn convert_string(kind: ConverterKind, input: &str) -> Result<String> {
+    Converter::new(kind).convert_string(input)
 }
 
 /// Convert a JsonValue array of 5e.tools entries to the Reroll equivalent.
-pub fn convert_json(kind: &str, input: JsonValue) -> Result<JsonValue> {
-    Converter::new(kind)?.convert_json(input)
+pub fn convert_json(kind: ConverterKind, input: JsonValue) -> Result<JsonValue> {
+    Converter::new(kind).convert_json(input)
 }
 
-/// Common methods and contract definition for Converter.
-pub trait Convert {
-    fn convert_file(&self, input_path: &str, output_path: &str) -> Result<()> {
+/// Converters to process 5e.tools data into Reroll data.
+pub enum ConverterKind {
+    Dummy,
+}
+
+pub struct Converter {
+    kind: ConverterKind,
+}
+
+impl Converter {
+    /// Create a new Converter of type 'kind'.
+    pub fn new(kind: ConverterKind) -> Converter {
+        Converter { kind }
+    }
+
+    pub fn convert_file(&self, input_path: &str, output_path: &str) -> Result<()> {
         let input_data = fs::read_to_string(input_path)?;
 
         let output_data = self.convert_string(&input_data)?;
@@ -34,7 +47,7 @@ pub trait Convert {
         Ok(())
     }
 
-    fn convert_string(&self, input: &str) -> Result<String> {
+    pub fn convert_string(&self, input: &str) -> Result<String> {
         let input_json = json::parse(input)?;
 
         let output_json = self.convert_json(input_json)?;
@@ -42,42 +55,17 @@ pub trait Convert {
         Ok(output_json.pretty(2))
     }
 
-    fn convert_json(&self, input: JsonValue) -> Result<JsonValue>;
-}
-
-/// Converters to process 5e.tools data into Reroll data.
-pub enum Converter {
-    Dummy,
-}
-
-impl Converter {
-    /// Create a new Converter of type 'kind'.
-    pub fn new(kind: &str) -> Result<Converter> {
-        match kind {
-            "dummy" => Ok(Converter::Dummy),
-            _ => Err(Error::UnknownKind(kind.to_string())),
-        }
-    }
-}
-
-impl Convert for Converter {
-    fn convert_json(&self, input: JsonValue) -> Result<JsonValue> {
-        match self {
-            Converter::Dummy => DummyConverter::new().convert_json(input),
+    pub fn convert_json(&self, input: JsonValue) -> Result<JsonValue> {
+        match self.kind {
+            ConverterKind::Dummy => DummyConverter.convert_json(input),
         }
     }
 }
 
 /// Internal implementation of Converter::Dummy.
-struct DummyConverter {}
+struct DummyConverter;
 
 impl DummyConverter {
-    fn new() -> DummyConverter {
-        DummyConverter {}
-    }
-}
-
-impl Convert for DummyConverter {
     fn convert_json(&self, input: JsonValue) -> Result<JsonValue> {
         Ok(input)
     }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,10 @@ fn main() -> retool::Result<()> {
     let input_path = "./data/input/backgrounds.json";
     let output_path = "./data/output/backgrounds.json";
 
-    retool::convert_file(data_kind, input_path, output_path)
+    let kind = match data_kind {
+        "dummy" => retool::ConverterKind::Dummy,
+        _ => panic!("Unknown kind: {}", data_kind),
+    };
+
+    retool::convert_file(kind, input_path, output_path)
 }
