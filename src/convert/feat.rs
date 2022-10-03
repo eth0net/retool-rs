787c200893be84a62d7a28a7eb3940153f1b4b8b
Elliot Thomas <elliot@eth0net.io>
2022-10-03 01:26:30 +0100

    chore: refactor for better iteration

--- src/convert/feat.rs
+++ src/convert/feat.rs
@@ -32,143 +32,10 @@ impl JsonConverter for FeatConverter {
 
 impl FeatConverter {
     fn prerequisite_to_string(&self, prerequisites: JsonValue) -> String {
-        let prerequisites = prerequisites
+        let prerequisites: Vec<String> = prerequisites
             .members()
             // todo: sort prerequisites by weight
-            .map(|prerequisite| {
-                let mut stack = vec![];
-
-                if let Some(other) = prerequisite["other"].as_str() {
-                    stack.push(other.to_string());
-                }
-
-                if let Some(level) = prerequisite["level"].as_usize() {
-                    let suffix = match level % 10 {
-                        1 => "st",
-                        2 => "nd",
-                        3 => "rd",
-                        _ => "th",
-                    };
-                    stack.push(format!("{}{} level", level, suffix));
-                }
-
-                if let JsonValue::Array(abilities) = &prerequisite["ability"] {
-                    let (abilities, level) = abilities
-                        .iter()
-                        .flat_map(|ability| ability.entries())
-                        .map(|ability| match ability.0 {
-                            "cha" => ("Charisma", ability.1),
-                            "con" => ("Constitution", ability.1),
-                            "dex" => ("Dexterity", ability.1),
-                            "int" => ("Intelligence", ability.1),
-                            "str" => ("Strength", ability.1),
-                            "wis" => ("Wisdom", ability.1),
-                            _ => ("Unknown", ability.1),
-                        })
-                        .fold((vec![], 0), |mut acc, ability| {
-                            acc.0.push(ability.0);
-                            if let Some(lvl) = ability.1.as_usize() {
-                                acc.1 = lvl;
-                            }
-                            acc
-                        });
-
-                    stack.push(format!("{} {} or higher", abilities.join(" or "), level))
-                }
-
-                if let JsonValue::Array(races) = &prerequisite["race"] {
-                    let to_name = |race: &JsonValue| {
-                        let name = match race["displayEntry"].is_string() {
-                            true => race["displayEntry"].to_string(),
-                            false => race["name"].to_string(),
-                        };
-
-                        match race["subrace"].is_string() {
-                            true => format!("{} ({})", name, race["subrace"]),
-                            false => name,
-                        }
-                    };
-
-                    let mut races = races.iter().map(to_name).collect::<Vec<String>>();
-
-                    if let Some(race) = races.get(0) {
-                        let to_title = |s: &str| {
-                            let (first, rest) = s.split_at(1);
-                            format!(
-                                "{}{}",
-                                first.to_ascii_uppercase(),
-                                rest.to_ascii_lowercase()
-                            )
-                        };
-
-                        races[0] = race
-                            .split('-')
-                            .map(to_title)
-                            .collect::<Vec<String>>()
-                            .join("-");
-                    }
-
-                    let len = races.len();
-                    let sep = match len > 2 {
-                        true => {
-                            races[len - 1].insert_str(0, "or ");
-                            ", "
-                        }
-                        false => " or ",
-                    };
-
-                    stack.push(races.join(sep));
-                }
-
-                if prerequisite["proficiency"].is_array() {
-                    let to_strings = |proficiency: &JsonValue| {
-                        proficiency
-                            .entries()
-                            .map(|(class, kind)| match class {
-                                "weapon" => format!("a {} {}", kind, class),
-                                _ => format!("{} {}", kind, class),
-                            })
-                            .collect::<Vec<String>>()
-                    };
-
-                    let join_strings = |mut v: Vec<String>| -> String {
-                        let mut sep = " and ";
-                        let len = v.len();
-                        if len > 2 {
-                            v[len - 1].insert_str(0, "and ");
-                            sep = ", ";
-                        }
-                        v.join(sep)
-                    };
-
-                    let mut proficiencies = prerequisite["proficiency"]
-                        .members()
-                        .map(to_strings)
-                        .map(join_strings)
-                        .collect::<Vec<String>>();
-
-                    let mut sep = " or ";
-                    let len = proficiencies.len();
-                    if len > 2 {
-                        proficiencies[len - 1].insert_str(0, "or ");
-                        sep = ", ";
-                    }
-
-                    stack.push(format!("Proficiency with {}", proficiencies.join(sep)));
-                }
-
-                if let Some(spellcasting) = prerequisite["spellcasting"].as_bool() {
-                    if spellcasting {
-                        stack.push("The ability to cast at least one spell".to_string())
-                    }
-                }
-
-                if let Some(spellcasting2020) = prerequisite["spellcasting2020"].as_bool() {
-                    if spellcasting2020 {
-                        stack.push("Spellcasting or Pact Magic feature".to_string())
-                    }
-                }
-
+            .filter_map(|prerequisite| {
                 // alignment
                 // background
                 // feat
@@ -177,10 +44,164 @@ impl FeatConverter {
                 //      weapon
                 // psionics
 
-                stack.join(", ")
-            })
-            .collect::<Vec<String>>();
+                prerequisite
+                    .entries()
+                    .filter_map(|(k, v)| match k {
+                        "note" => None,
+                        "level" => {
+                            if let Some(lvl) = v.as_u8() {
+                                let suffix = ordinal_form(lvl);
+                                return Some(format!("{} level", suffix));
+                            }
+                            if let Some(lvl) = v["level"].as_u8() {
+                                let suffix = ordinal_form(lvl);
+                                return Some(format!("{} level", suffix));
+                            }
+                            None
+                        }
+                        "race" => {
+                            let mut races = v
+                                .members()
+                                .filter_map(|race| {
+                                    let display_entry = race["displayEntry"].as_str();
+                                    let name = race["name"].as_str();
+                                    let subrace = race["subrace"].as_str();
 
-        format!("Prerequisite: {}", prerequisites.join(", "))
+                                    display_entry.or(name).map(|race| (race, subrace))
+                                })
+                                .map(|(race, subrace)| (title_case(race), subrace))
+                                .map(|(race, subrace)| match subrace {
+                                    Some(subrace) => format!("{} ({})", race, subrace),
+                                    None => race,
+                                })
+                                .collect::<Vec<String>>();
+
+                            let len = races.len();
+                            let sep = match len > 2 {
+                                true => {
+                                    races[len - 1].insert_str(0, "or ");
+                                    ", "
+                                }
+                                false => " or ",
+                            };
+
+                            Some(races.join(sep))
+                        }
+                        "ability" => {
+                            let (abilities, level) = v
+                                .members()
+                                .flat_map(|ability| ability.entries())
+                                .filter_map(|(ability, level)| match ability {
+                                    "cha" => Some(("Charisma", level)),
+                                    "con" => Some(("Constitution", level)),
+                                    "dex" => Some(("Dexterity", level)),
+                                    "int" => Some(("Intelligence", level)),
+                                    "str" => Some(("Strength", level)),
+                                    "wis" => Some(("Wisdom", level)),
+                                    _ => None,
+                                })
+                                .fold((vec![], 0), |mut acc, (ability, level)| {
+                                    acc.0.push(ability);
+                                    if let Some(lvl) = level.as_usize() {
+                                        acc.1 = lvl;
+                                    }
+                                    acc
+                                });
+
+                            match !abilities.is_empty() {
+                                true => {
+                                    Some(format!("{} {} or higher", abilities.join(" or "), level))
+                                }
+                                false => None,
+                            }
+                        }
+                        "proficiency" => {
+                            let mut proficiencies = v
+                                .members()
+                                .map(|proficiency| {
+                                    proficiency
+                                        .entries()
+                                        .map(|(class, kind)| match class {
+                                            "weapon" => format!("a {} {}", kind, class),
+                                            _ => format!("{} {}", kind, class),
+                                        })
+                                        .collect::<Vec<String>>()
+                                })
+                                .map(|mut v| {
+                                    let mut sep = " and ";
+                                    let len = v.len();
+                                    if len > 2 {
+                                        v[len - 1].insert_str(0, "and ");
+                                        sep = ", ";
+                                    }
+                                    v.join(sep)
+                                })
+                                .collect::<Vec<String>>();
+
+                            let mut sep = " or ";
+                            let len = proficiencies.len();
+                            if len > 2 {
+                                proficiencies[len - 1].insert_str(0, "or ");
+                                sep = ", ";
+                            }
+
+                            Some(format!("Proficiency with {}", proficiencies.join(sep)))
+                        }
+                        "spellcasting" => match v.as_bool() {
+                            Some(true) => {
+                                Some(String::from("The ability to cast at least one spell"))
+                            }
+                            _ => None,
+                        },
+                        "spellcasting2020" => match v.as_bool() {
+                            Some(true) => Some(String::from("Spellcasting or Pact Magic feature")),
+                            _ => None,
+                        },
+                        "other" => v.as_str().map(|other| other.to_string()),
+                        _ => None,
+                    })
+                    .reduce(|a, i| match i.contains(" or ") {
+                        true => format!("{}; {}", a, i),
+                        false => format!("{}, {}", a, i),
+                    })
+            })
+            .collect();
+
+        let prefix = match prerequisites.len() {
+            1 => "Prerequisite: ",
+            _ => "Prerequisites: ",
+        };
+
+        format!("{}{}", prefix, prerequisites.join(", "))
     }
+}
+
+fn ordinal_form(i: u8) -> String {
+    let m = (i % 10, i % 100);
+    let suffix = if m.0 == 1 && m.1 != 11 {
+        "st"
+    } else if m.0 == 2 && m.1 != 12 {
+        "nd"
+    } else if m.0 == 3 && m.1 != 13 {
+        "rd"
+    } else {
+        "th"
+    };
+    format!("{}{}", i, suffix)
+}
+
+fn title_case(s: &str) -> String {
+    let to_title = |s: &str| {
+        let (first, rest) = s.split_at(1);
+        format!(
+            "{}{}",
+            first.to_ascii_uppercase(),
+            rest.to_ascii_lowercase()
+        )
+    };
+
+    s.split('-')
+        .map(to_title)
+        .collect::<Vec<String>>()
+        .join("-")
 }
