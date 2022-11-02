403eca3cfb19e04fe4598eaaeab5af74a3a01c08
Elliot Thomas <elliot@eth0net.io>
2022-11-02 17:22:45 +0000

    chore: refactor prerequisite logic into new module

--- src/convert/feat.rs
+++ src/convert/feat.rs
@@ -1,7 +1,7 @@
 use anyhow::{bail, Result};
 use json::{array, object, JsonValue};
 
-use super::*;
+use super::JsonConverter;
 
 pub struct FeatConverter;
 
@@ -15,7 +15,7 @@ impl JsonConverter for FeatConverter {
             .members()
             .map(|feat| {
                 let mut desc = String::new();
-                desc.push_str(&self.prerequisite_to_string(feat["prerequisite"].clone()));
+                desc.push_str(&prerequisite::to_string(feat["prerequisite"].clone()));
 
                 object! {
                     name: feat["name"].clone(),
@@ -30,8 +30,12 @@ impl JsonConverter for FeatConverter {
     }
 }
 
-impl FeatConverter {
-    fn prerequisite_to_string(&self, prerequisites: JsonValue) -> String {
+mod prerequisite {
+    use crate::convert::util::{join_conjunct, ordinal_form, title_case};
+
+    use json::JsonValue;
+
+    pub(crate) fn to_string(prerequisites: JsonValue) -> String {
         let prerequisites: Vec<String> = prerequisites
             .members()
             .filter_map(|prerequisite| {
@@ -48,114 +52,13 @@ impl FeatConverter {
                     .entries()
                     .filter_map(|(k, v)| match k {
                         "note" => None,
-                        "level" => {
-                            if let Some(level) = v.as_u8() {
-                                return Some(format!("{} level", ordinal_form(level)));
-                            };
-
-                            if let JsonValue::Object(level) = v {
-                                let level_string = level["level"]
-                                    .as_u8()
-                                    .map(|l| format!("{} level", ordinal_form(l)));
-
-                                let class_name = level["class"]["name"].as_str();
-                                let subclass_name = level["subclass"]["name"].as_str();
-
-                                let level_visible = level["level"] != 1;
-                                let subclass_visible = level["subclass"]["visible"] == true;
-                                let class_visible =
-                                    level["class"]["visible"] == true || subclass_visible;
-
-                                let class_string: Option<String> = None;
-
-                                if level_string.is_some() && class_string.is_some() {
-                                    return Some(format!(
-                                        "{} {}",
-                                        level_string.unwrap(),
-                                        class_string.unwrap()
-                                    ));
-                                } else if level_string.is_some() {
-                                    return level_string;
-                                } else if class_string.is_some() {
-                                    return class_string;
-                                }
-                            };
-
-                            None
-                        }
-                        "race" => {
-                            let races = v
-                                .members()
-                                .filter_map(|race| {
-                                    let display_entry = race["displayEntry"].as_str();
-                                    let name = race["name"].as_str();
-                                    let subrace = race["subrace"].as_str();
-
-                                    display_entry.or(name).map(|race| (race, subrace))
-                                })
-                                .map(|(race, subrace)| (title_case(race), subrace))
-                                .map(|(race, subrace)| match subrace {
-                                    Some(subrace) => format!("{} ({})", race, subrace),
-                                    None => race,
-                                })
-                                .collect::<Vec<String>>();
-
-                            join_conjunct(races, ", ", "or ")
-                        }
-                        "ability" => {
-                            let (abilities, level) = v
-                                .members()
-                                .flat_map(|ability| ability.entries())
-                                .filter_map(|(ability, level)| match ability {
-                                    "cha" => Some(("Charisma", level)),
-                                    "con" => Some(("Constitution", level)),
-                                    "dex" => Some(("Dexterity", level)),
-                                    "int" => Some(("Intelligence", level)),
-                                    "str" => Some(("Strength", level)),
-                                    "wis" => Some(("Wisdom", level)),
-                                    _ => None,
-                                })
-                                .fold((vec![], 0), |mut acc, (ability, level)| {
-                                    acc.0.push(ability.to_string());
-                                    if let Some(lvl) = level.as_usize() {
-                                        acc.1 = lvl;
-                                    }
-                                    acc
-                                });
-
-                            join_conjunct(abilities, ", ", "or ")
-                                .map(|a| format!("{} {} or higher", a, level))
-                        }
-                        "proficiency" => {
-                            let proficiencies = v
-                                .members()
-                                .filter_map(|proficiency| {
-                                    let entries = proficiency
-                                        .entries()
-                                        .filter_map(|(class, kind)| match class {
-                                            "armor" => Some(format!("{} {}", kind, class)),
-                                            "weapon" => Some(format!("a {} {}", kind, class)),
-                                            _ => None,
-                                        })
-                                        .collect::<Vec<String>>();
-                                    join_conjunct(entries, ", ", "and ")
-                                })
-                                .collect::<Vec<String>>();
-
-                            join_conjunct(proficiencies, ", ", "or ")
-                                .map(|p| format!("Proficiency with {}", p))
-                        }
-                        "spellcasting" => match v.as_bool() {
-                            Some(true) => {
-                                Some("The ability to cast at least one spell".to_string())
-                            }
-                            _ => None,
-                        },
-                        "spellcasting2020" => match v.as_bool() {
-                            Some(true) => Some("Spellcasting or Pact Magic feature".to_string()),
-                            _ => None,
-                        },
-                        "other" => v.as_str().map(|other| other.to_string()),
+                        "level" => level_string(v),
+                        "race" => race_string(v),
+                        "ability" => ability_string(v),
+                        "proficiency" => proficiency_string(v),
+                        "spellcasting" => spellcasting_string(v),
+                        "spellcasting2020" => spellcasting2020_string(v),
+                        "other" => other_to_string(v),
                         _ => None,
                     })
                     .reduce(|a, i| match i.contains(" or ") {
@@ -173,44 +76,115 @@ impl FeatConverter {
 
         format!("{}{}", prefix, prerequisites.join(", "))
     }
-}
 
-fn ordinal_form(i: u8) -> String {
-    let m = (i % 10, i % 100);
-    let suffix = if m.0 == 1 && m.1 != 11 {
-        "st"
-    } else if m.0 == 2 && m.1 != 12 {
-        "nd"
-    } else if m.0 == 3 && m.1 != 13 {
-        "rd"
-    } else {
-        "th"
-    };
-    format!("{}{}", i, suffix)
-}
+    fn other_to_string(v: &JsonValue) -> Option<String> {
+        v.as_str().map(|other| other.to_string())
+    }
 
-fn title_case(s: &str) -> String {
-    let to_title = |s: &str| {
-        let (first, rest) = s.split_at(1);
-        format!(
-            "{}{}",
-            first.to_ascii_uppercase(),
-            rest.to_ascii_lowercase()
-        )
-    };
+    fn spellcasting2020_string(v: &JsonValue) -> Option<String> {
+        match v.as_bool() {
+            Some(true) => Some("Spellcasting or Pact Magic feature".to_string()),
+            _ => None,
+        }
+    }
 
-    s.split('-')
-        .map(to_title)
-        .collect::<Vec<String>>()
-        .join("-")
-}
+    fn spellcasting_string(v: &JsonValue) -> Option<String> {
+        match v.as_bool() {
+            Some(true) => Some("The ability to cast at least one spell".to_string()),
+            _ => None,
+        }
+    }
 
-fn join_conjunct(v: Vec<String>, s1: &str, s2: &str) -> Option<String> {
-    v.iter()
-        .enumerate()
-        .map(|(i, s)| match i == v.len() - 1 {
-            true => format!("{}{}", s1, s),
-            false => format!("{}{}{}", s1, s2, s),
-        })
-        .reduce(|a, i| format!("{}{}", a, i))
+    fn proficiency_string(v: &JsonValue) -> Option<String> {
+        let proficiencies = v
+            .members()
+            .filter_map(|proficiency| {
+                let entries = proficiency
+                    .entries()
+                    .filter_map(|(class, kind)| match class {
+                        "armor" => Some(format!("{} {}", kind, class)),
+                        "weapon" => Some(format!("a {} {}", kind, class)),
+                        _ => None,
+                    })
+                    .collect::<Vec<String>>();
+                join_conjunct(entries, ", ", "and ")
+            })
+            .collect::<Vec<String>>();
+        join_conjunct(proficiencies, ", ", "or ").map(|p| format!("Proficiency with {}", p))
+    }
+
+    fn ability_string(v: &JsonValue) -> Option<String> {
+        let (abilities, level) = v
+            .members()
+            .flat_map(|ability| ability.entries())
+            .filter_map(|(ability, level)| match ability {
+                "cha" => Some(("Charisma", level)),
+                "con" => Some(("Constitution", level)),
+                "dex" => Some(("Dexterity", level)),
+                "int" => Some(("Intelligence", level)),
+                "str" => Some(("Strength", level)),
+                "wis" => Some(("Wisdom", level)),
+                _ => None,
+            })
+            .fold((vec![], 0), |mut acc, (ability, level)| {
+                acc.0.push(ability.to_string());
+                if let Some(lvl) = level.as_usize() {
+                    acc.1 = lvl;
+                }
+                acc
+            });
+        join_conjunct(abilities, ", ", "or ").map(|a| format!("{} {} or higher", a, level))
+    }
+
+    fn race_string(v: &JsonValue) -> Option<String> {
+        let races = v
+            .members()
+            .filter_map(|race| {
+                let display_entry = race["displayEntry"].as_str();
+                let name = race["name"].as_str();
+                let subrace = race["subrace"].as_str();
+
+                display_entry.or(name).map(|race| (race, subrace))
+            })
+            .map(|(race, subrace)| (title_case(race), subrace))
+            .map(|(race, subrace)| match subrace {
+                Some(subrace) => format!("{} ({})", race, subrace),
+                None => race,
+            })
+            .collect::<Vec<String>>();
+        join_conjunct(races, ", ", "or ")
+    }
+
+    fn level_string(v: &JsonValue) -> Option<String> {
+        if let Some(level) = v.as_u8() {
+            return Some(format!("{} level", ordinal_form(level)));
+        };
+        if let JsonValue::Object(level) = v {
+            let level_string = level["level"]
+                .as_u8()
+                .map(|l| format!("{} level", ordinal_form(l)));
+
+            let class_name = level["class"]["name"].as_str();
+            let subclass_name = level["subclass"]["name"].as_str();
+
+            let level_visible = level["level"] != 1;
+            let subclass_visible = level["subclass"]["visible"] == true;
+            let class_visible = level["class"]["visible"] == true || subclass_visible;
+
+            let class_string: Option<String> = None;
+
+            if level_string.is_some() && class_string.is_some() {
+                return Some(format!(
+                    "{} {}",
+                    level_string.unwrap(),
+                    class_string.unwrap()
+                ));
+            } else if level_string.is_some() {
+                return level_string;
+            } else if class_string.is_some() {
+                return class_string;
+            }
+        };
+        None
+    }
 }
---
+++ src/convert/util.rs
@@ -0,0 +1,39 @@
+pub(crate) fn ordinal_form(i: u8) -> String {
+    let m = (i % 10, i % 100);
+    let suffix = if m.0 == 1 && m.1 != 11 {
+        "st"
+    } else if m.0 == 2 && m.1 != 12 {
+        "nd"
+    } else if m.0 == 3 && m.1 != 13 {
+        "rd"
+    } else {
+        "th"
+    };
+    format!("{}{}", i, suffix)
+}
+
+pub(crate) fn title_case(s: &str) -> String {
+    let to_title = |s: &str| {
+        let (first, rest) = s.split_at(1);
+        format!(
+            "{}{}",
+            first.to_ascii_uppercase(),
+            rest.to_ascii_lowercase()
+        )
+    };
+
+    s.split('-')
+        .map(to_title)
+        .collect::<Vec<String>>()
+        .join("-")
+}
+
+pub(crate) fn join_conjunct(v: Vec<String>, s1: &str, s2: &str) -> Option<String> {
+    v.iter()
+        .enumerate()
+        .map(|(i, s)| match i == v.len() - 1 {
+            true => format!("{}{}", s1, s),
+            false => format!("{}{}{}", s1, s2, s),
+        })
+        .reduce(|a, i| format!("{}{}", a, i))
+}
