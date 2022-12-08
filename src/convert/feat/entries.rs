32bd4736db71d344bfe3e4e0aff747139438a35d
Elliot Thomas <elliot@eth0net.io>
2022-12-08 22:57:19 +0000

    feat: finish entries list and item

--- src/convert/feat/entries.rs
+++ src/convert/feat/entries.rs
@@ -11,27 +11,62 @@ pub(crate) fn to_string(entries: JsonValue) -> Option<String> {
 
 fn entry_to_string(entry: &JsonValue) -> Option<String> {
     match entry {
+        JsonValue::Short(e) => Some(e.to_string()),
         JsonValue::String(e) => Some(e.to_string()),
         JsonValue::Object(e) => match &e["type"] {
             t if t == "entries" => None,
-            t if t == "list" => list_to_string(&e["items"]),
+            t if t == "item" => item_to_string(entry),
+            t if t == "list" => list_to_string(entry),
             t if t == "section" => None,
             t if t == "table" => None,
             _ => None,
         },
+        JsonValue::Array(_) => {
+            let entries: Vec<String> = entry.members().filter_map(entry_to_string).collect();
+            match entries.is_empty() {
+                false => Some(entries.join("\n")),
+                true => None,
+            }
+        }
         _ => None,
     }
 }
 
-fn list_to_string(items: &JsonValue) -> Option<String> {
-    let items: Vec<String> = items
+fn item_to_string(item: &JsonValue) -> Option<String> {
+    let mut stack = vec![];
+
+    if let Some(name) = entry_to_string(&item["name"]) {
+        stack.push(format!("{}.", name));
+    }
+
+    if let Some(entry) = entry_to_string(&item["entry"]) {
+        stack.push(entry);
+    }
+
+    if let Some(entries) = entry_to_string(&item["entries"]) {
+        stack.push(entries);
+    }
+
+    match stack.is_empty() {
+        false => Some(stack.join(" ")),
+        true => None,
+    }
+}
+
+fn list_to_string(list: &JsonValue) -> Option<String> {
+    let mut stack = vec![];
+
+    if let Some(name) = entry_to_string(&list["name"]) {
+        stack.push(name);
+    }
+
+    list["items"]
         .members()
         .filter_map(entry_to_string)
-        .map(|i| format!("- {}", i))
-        .collect();
+        .for_each(|i| stack.push(i));
 
-    match items.is_empty() {
-        false => Some(items.join("\n")),
+    match stack.is_empty() {
+        false => Some(stack.join("\n")),
         true => None,
     }
 }
