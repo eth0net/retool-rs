2d70f33a2cb7d0c500cc13de5fe01169e886d260
Elliot Thomas <elliot@eth0net.io>
2022-12-08 16:40:46 +0000

    chore: simplify entries mod and improve list join

--- src/convert/feat/entries.rs
+++ src/convert/feat/entries.rs
@@ -1,7 +1,7 @@
 use json::JsonValue;
 
 pub(crate) fn to_string(entries: JsonValue) -> Option<String> {
-    let entries: Vec<String> = entries.members().filter_map(map_entries).collect();
+    let entries: Vec<String> = entries.members().filter_map(entry_to_string).collect();
 
     match entries.is_empty() {
         false => Some(entries.join("\n")),
@@ -9,33 +9,29 @@ pub(crate) fn to_string(entries: JsonValue) -> Option<String> {
     }
 }
 
-fn map_entries(entry: &JsonValue) -> Option<String> {
-    let mut stack = vec![];
-
+fn entry_to_string(entry: &JsonValue) -> Option<String> {
     match entry {
-        JsonValue::String(e) => stack.push(e.to_string()),
-        JsonValue::Object(e) => {
-            match e["type"].to_string() {
-                t if t == "entries" => (),
-                t if t == "list" => stack.push(list_to_string(&e["items"])),
-                t if t == "section" => (),
-                t if t == "table" => (),
-                _ => (),
-            };
-        }
-        _ => (),
-    }
-
-    match stack.is_empty() {
-        false => Some(stack.join("\n")),
-        true => None,
+        JsonValue::String(e) => Some(e.to_string()),
+        JsonValue::Object(e) => match &e["type"] {
+            t if t == "entries" => None,
+            t if t == "list" => list_to_string(&e["items"]),
+            t if t == "section" => None,
+            t if t == "table" => None,
+            _ => None,
+        },
+        _ => None,
     }
 }
 
-fn list_to_string(items: &JsonValue) -> String {
-    items
+fn list_to_string(items: &JsonValue) -> Option<String> {
+    let items: Vec<String> = items
         .members()
-        .filter_map(|m| m.as_str())
-        .collect::<Vec<&str>>()
-        .join(",")
+        .filter_map(entry_to_string)
+        .map(|i| format!("- {}", i))
+        .collect();
+
+    match items.is_empty() {
+        false => Some(items.join("\n")),
+        true => None,
+    }
 }
